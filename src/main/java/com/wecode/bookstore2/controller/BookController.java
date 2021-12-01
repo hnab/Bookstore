c6f5528843f2effb1b6e9a6d8afca82c967f0c0e
hatef <hatef.nabili@gmail.com>
2021-11-30 22:44:51 -0800

    first

---
+++ src/main/java/com/wecode/bookstore2/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.wecode.bookstore2.controller;
+
+import com.wecode.bookstore2.controller.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks(){
+        BookDto book= BookDto.builder().title("My first").build();
+
+        List<BookDto> books=new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+    }
+}
---
+++ src/test/java/com/wecode/bookstore2/BookStore2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecode.bookstore2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStore2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
